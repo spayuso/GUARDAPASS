01e40142b33f9e462a0b826e5c6701e5505193ba
pajueloayuso <pajueloayuso@gmail.com>
2016-07-01 13:12:36 +0200

    Metodos definidos, dos implementados

--- GuardaPass/GuardaPass/Controller.cs
+++ GuardaPass/GuardaPass/Controller.cs
@@ -8,5 +8,36 @@ namespace GuardaPass
 {
     class Controller
     {
+        public String nombreUsuario { get; set; };
+        public String password { get; set; };
+
+
+        public void CrearNuevoUsuario(String nom, String pass)
+        {
+            Console.WriteLine("Introduzca nombre de usuario");
+            nombreUsuario = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Introduzca password de usuario");
+            password = Convert.ToString(Console.ReadLine());
+        }
+
+        public Boolean Login(String nom, String pass)
+        {
+            if (nom == nombreUsuario && pass == password)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public Boolean AnadirContrasena()
+        {
+
+        }
+
+        public Boolean ListarContrasena()
+        {
+
+        }
     }
 }
